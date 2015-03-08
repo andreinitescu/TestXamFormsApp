4187a9092780b7d7bac0f0ee574ce3da1aad0cc2
Andrei Nitescu <andrei.nitescu@infor.com>
2015-03-08 15:02:57 +0200

    border test

--- Droid/Renderers/BorderRenderer.cs
+++ Droid/Renderers/BorderRenderer.cs
@@ -7,6 +7,8 @@ using Android.Views;
 using Android.Graphics.Drawables;
 using Android.Graphics;
 using Android.Widget;
+using System.Linq;
+using System.ComponentModel;
 
 [assembly:ExportRendererAttribute(typeof(Border), typeof(BorderRenderer))]
 
@@ -14,11 +16,27 @@ namespace TestXamFormsApp.Droid.Renderers
 {
 	public class BorderRenderer : VisualElementRenderer<Border>
 	{
-		public BorderRenderer()
+		protected override void OnElementChanged (ElementChangedEventArgs<Border> e)
 		{
-			this.SetBackgroundColor (Android.Graphics.Color.Red);
-			//this.SetBackgroundResource(Resource.Drawable.shape);
-			this.SetPadding (200, 70, 70, 70);
+			base.OnElementChanged (e);
+			UpdateBackground ();
+		}
+
+		void UpdateBackground()
+		{
+			var d = new GradientDrawable ();
+
+			// set drawable color based on Border's color
+			d.SetColor (Element.BackgroundColor.ToAndroid ()); 
+
+			// set drawable stroke thickness and color based on Border's stroke thickiness
+			d.SetStroke ((int)Context.ToPixels (Element.StrokeThickness), Android.Graphics.Color.Black); 
+
+			// set padding to take into account the drawable stroke thickiness
+			SetPadding ((int)Element.StrokeThickness, (int)Element.StrokeThickness, (int)Element.StrokeThickness, (int)Element.StrokeThickness);
+
+			// set drawable
+			SetBackgroundDrawable (d); 
 		}
 	}
 }
--- TestXamFormsApp/Controls/Border.cs
+++ TestXamFormsApp/Controls/Border.cs
@@ -5,6 +5,21 @@ namespace TestXamFormsApp.Controls
 {
 	public class Border : ContentView	
 	{
+		/*public static readonly BindableProperty StrokeProperty = 
+			BindableProperty.Create<Border,Color>(p => p.Stroke, Color.Transparent);
+
+		public Color Stroke {
+			get { return (Color)GetValue(StrokeProperty); }
+			set { SetValue(StrokeProperty, value); }
+		}*/
+
+		public static readonly BindableProperty StrokeThicknessProperty = 
+			BindableProperty.Create<Border,double>(p => p.StrokeThickness, default(double));
+
+		public double StrokeThickness {
+			get { return (double)GetValue (StrokeThicknessProperty); }
+			set { SetValue (StrokeThicknessProperty, value); }
+		}
 	}
 }
 
